a595389df314cce48e6473b210c5f9b93a1d052b
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-06-22 19:47:49 +0100

    Create Folders and Files Structure

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title> PurpleBeeTech- Custom Software & App Development Company</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Custom Software & App Development Company" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
